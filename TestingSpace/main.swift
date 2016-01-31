947fa4bb5d50fee6178c043a73b4a1a526e55d33
armen <absoboll@gmail.com>
2016-01-30 21:44:32 -0800

    Initial Commit

---
+++ TestingSpace/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestingSpace
+//
+//  Created by armen karamian on 1/30/16.
+//  Copyright © 2016 armen karamian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
